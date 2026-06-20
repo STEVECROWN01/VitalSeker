fdd79abc0beabc52db8804e3d08defaa33d9bf28
VitalSeker Dev <vitalseker-dev@ketermarketing.com>
2026-06-20 17:48:21 +0000

    fix: compileSdk back to 36 (plugins app_links + geolocator require it)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.ketermarketing.vitalseker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ketermarketing.vitalseker"
