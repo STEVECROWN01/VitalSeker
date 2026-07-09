c8d16c44ef0bd1ef573e9d6f197a7e839d6c1bfb
Stevens Akpovi <stevensakpovi@gmail.com>
2026-07-09 13:45:28 +0100

    cleanup: remove all unused imports, fields, and elements (26 fixes)

--- lib/features/profile/screens/pro_plan_screen.dart
+++ lib/features/profile/screens/pro_plan_screen.dart
@@ -5,7 +5,6 @@ import '../../../core/config/app_config.dart';
 import '../../../core/providers/auth_provider.dart';
 import '../../../core/providers/subscription_provider.dart';
 import '../../../core/providers/user_profile_provider.dart';
-import '../../../core/services/database_service.dart';
 import '../../../shared/theme/app_colors.dart';
 import '../../../shared/theme/app_text_styles.dart';
 import '../../../shared/widgets/app_snack_bar.dart';
