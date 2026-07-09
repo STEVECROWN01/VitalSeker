577ab49bf8f77c3db0a5d68b1525643478ca89a3
Stevens Akpovi <stevensakpovi@gmail.com>
2026-07-09 13:45:12 +0100

    cleanup: remove all unused imports, fields, and elements (26 fixes)

--- lib/core/providers/symptom_log_provider.dart
+++ lib/core/providers/symptom_log_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/symptom_log.dart';
-import '../services/database_service.dart';
 import '../services/offline_cache_service.dart';
 import 'auth_provider.dart';
 import 'user_profile_provider.dart';
