678fa34a2344adabe6424dee61cd3ef211e5dd4c
Stevens Akpovi <stevensakpovi@gmail.com>
2026-07-10 19:30:53 +0100

    fix: all remaining errors + warnings (2 errors + 17 warnings to 0)

--- lib/features/profile/screens/subscription_screen.dart
+++ lib/features/profile/screens/subscription_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vitalseker/l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../../core/services/revenuecat_service.dart';
 import '../../../core/providers/subscription_provider.dart';
